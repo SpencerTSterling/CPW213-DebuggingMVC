98f0d0397e2f456a60b2ae9278a99d8594e7b808
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-08-19 10:31:35 -0700

    Added ProductDb class to handle database code

---
+++ AspnetCoreWithBugs/Data/ProductDb.cs
@@ -0,0 +1,26 @@
+﻿using AspnetCoreWithBugs.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreWithBugs.Data
+{
+    public static class ProductDb
+    {
+        /// <summary>
+        /// Gets all the products from the database and returns a list
+        /// </summary>
+        /// <param name="_context"></param>
+        /// <returns></returns>
+        public async static Task<List<Product>> ListProductsAysnc(ProductContext _context)
+        {
+            return await
+                (from p in _context.Product
+                 orderby p.Name ascending
+                 select p).ToListAsync();
+        }
+
+    }
+}
